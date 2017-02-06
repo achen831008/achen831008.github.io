e34d6de58e38cfb58a1f61256963f42100a1ec2a
Alex Chen <alexchen@Alexs-MacBook-Pro.local>
2017-02-05 23:38:01 -0500

    rewriting site using angular

--- js/app.js
+++ js/app.js
@@ -1,78 +1,54 @@
-window.Instagram = {
-    /**
-     * Store application settings
-     */
-    config: {},
+var app = angular
+        .module('mySite', ['ngRoute'])
+        .config(config);
 
-    BASE_URL: 'https://api.instagram.com/v1',
+/* config */
+function config($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+                templateUrl : 'views/main.html',
+                controller  : 'mainController'
+        })
+        .when('/about', {
+                templateUrl : 'views/about.html',
+                controller  : 'aboutController'
+        })
+        .when('/experience', {
+                templateUrl : 'views/experience.html',
+                controller  : 'experienceController'
+        })
+        .when('/project', {
+                templateUrl : 'views/project.html',
+                controller  : 'projectController'               
+        })
+        .otherwise('/');
 
-    init: function( opt ) {
-        opt = opt || {};
+        $locationProvider.html5Mode(true);
+}
 
-        this.config.client_id = opt.client_id;
-    },
-
-    /**
-     * Get a list of popular media.
-     */
-    popular: function( callback ) {
-        var endpoint = this.BASE_URL + '/media/popular?client_id=' + this.config.client_id;
-        this.getJSON( endpoint, callback );
-    },
-
-    /**
-     * Get a list of recently tagged media.
-     */
-    tagsByName: function( name, callback ) {
-        var endpoint = this.BASE_URL + '/tags/' + name + '/media/recent?client_id=' + this.config.client_id;
-        this.getJSON( endpoint, callback );
-    },
-
-    getJSON: function( url, callback ) {
-        $.ajax({
-            type: 'GET',
-            url: url,
-            dataType: 'jsonp',
-            success: function( response ) {
-                if ( typeof callback === 'function' ) callback( response );
-            }
+/* controller */
+masterController.$inject = ['$scope', '$rootScope', '$location', '$sce'];
+function masterController($scope, $rootScope, $location, $sce) {
+        $scope.Name = 'Alex Chen';
+        $scope.copyright = $sce.trustAsHtml("Copyright &copy; 2017 Alex Chen");
+        $scope.menu = [
+                {label:'Home', route:'/'},
+                {label:'About', route:'/about'},
+                {label:'Experience', route:'/experience'},
+                {label:'Projects', route:'/project'}
+        ]
+        $scope.menuActive = '/';
+        $rootScope.$on('$routeChangeSuccess', function(e, curr, prev) {
+                $scope.menuActive = $location.path();
         });
-    }
-};
+}
 
-Instagram.init({
-    //client_id: 'd49da08a520f47cbb6e7618f077f33ef',
-    //code=1502d25cb5ec41e88cb8db74351731a8
-    //accessToken=214634784.8a868cf.30c12fb2fb8c471dac6c956f4f57dfa4
-    //userId=214634784
-    //accessToken: '214634784.8a868cf.30c12fb2fb8c471dac6c956f4f57dfa4'
-});
+/* controller */
+mainController.$inject = ['$scope'];
+function mainController($scope) {
+        var vm = this;
+        $scope.role = role = 'Software Engineering';
+}
 
+/* controller */
 
-$( document ).ready(function() {
-
-    Instagram.popular(function( response ) {
-        var $instagram = $( '#instagram' );
-        for ( var i = 0; i < response.data.length; i++ ) {
-            imageUrl = response.data[i].images.low_resolution.url;
-            $instagram.append( '<img src="' + imageUrl + '" />' );
-        }
-    });
-
-    $( '#form' ).on('submit', function( e ) {
-        e.preventDefault();
-
-        var tagName = $( '#search' ).val();
-        Instagram.tagsByName(tagName, function( response ) {
-            var $instagram = $( '#instagram' );
-                $instagram.html('');
-
-            for ( var i = 0; i < response.data.length; i++ ) {
-                imageUrl = response.data[i].images.low_resolution.url;
-                $instagram.append( '<img src="' + imageUrl + '" />' );
-            }
-        });
-
-    });
-
-});
