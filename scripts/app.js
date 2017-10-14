5256205880ed8f6e05c333b9195a39cfc6edb98e
Alex Chen <achen831008@gmail.com>
2017-10-13 21:10:53 -0400

    update new ui design

--- scripts/app.js
+++ scripts/app.js
@@ -2,30 +2,5 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'wt.responsive', 'app.services', 'ui.bootstrap'])
-        .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl : 'views/main.html',
-                    controller  : 'mainController',
-                    controllerAs : 'vm'
-                })
-                .when('/about', {
-                    templateUrl : 'views/about.html',
-                    controller  : 'aboutController'
-                })
-                .when('/experience', {
-                    templateUrl : 'views/experience.html',
-                    controller  : 'experienceController',
-                    controllerAs : 'vm'
-                })
-                .when('/project', {
-                    templateUrl : 'views/project.html',
-                    controller  : 'projectController',
-                    controllerAs : 'vm'
-                })
-                .otherwise({ redirectTo: '/' });
-
-            $locationProvider.html5Mode(true);
-        }]);
+        .module('app', ['ngRoute', 'ngAnimate']);
 })();
--- scripts/controllers/app.controller.js
+++ scripts/controllers/app.controller.js
@@ -5,38 +5,22 @@
         .module('app')
         .controller('appController', appController);
 
-    appController.$inject = ['$scope', '$rootScope', '$location', '$sce', '$window'];
+    appController.$inject = ['$scope', '$http', '$sce'];
 
-    function appController($scope, $rootScope, $location, $sce, $window) {
-        $scope.initScope = function () {
-            $scope.Name = 'Alex Chen';
-            $scope.copyright = $sce.trustAsHtml("Copyright &copy; 2017 Alex Chen");
-            $scope.menu = [
-                {label: 'Home', route: '/', icon: 'fa-home'},
-                {label: 'About', route: '/about', icon: 'fa-user'},
-                {label: 'Experience', route: '/experience', icon: 'fa-tasks'},
-                {label: 'Github', route: 'https://github.com/achen831008', icon: 'fa-github'},
-                {label: 'LinkedIn', route: 'https://ca.linkedin.com/in/achen831008', icon: 'fa-linkedin'}
-            ];
-
-            $scope.menuActive = '/';
-            $rootScope.$on('$routeChangeSuccess', function (e, curr, prev) {
-                $scope.menuActive = $location.path();
-            });
-            $scope.navStyle = ($window.innerWidth > 600);
-        }
-
-        $scope.setActive = function (label) {
-            $scope.menu.forEach(function (element) {
-                if (label === element.label) {
-                    angular.element('#' + element.label).addClass("active");
-                }
-                else {
-                    angular.element('#' + element.label).removeClass("active");
-                }
-            })
-        };
-
-        $scope.initScope();
+    function appController($scope, $http, $sce) {
+        //https://api.instagram.com/v1/users/self/media/recent/?access_token=6214268740.c67ca99.7925875186c8449c84fdfe7cfea728d9
+        var instagram_endpoint = 'https://api.instagram.com/v1/users/self/media/recent/';
+        var instagram_api_access_token = '6214268740.c67ca99.7925875186c8449c84fdfe7cfea728d9';
+        var request_url = instagram_endpoint + '?access_token=' + instagram_api_access_token;
+        
+        request_url = $sce.trustAsResourceUrl(request_url);
+        
+        $http.get('/data/v2/app.json').then(function(rsp){
+            $scope.data = rsp.data;
+        });
+        
+        $http.jsonp(request_url).then(function (rsp){
+            $scope.feeds = rsp.data.data;
+        });
     }
 })();
