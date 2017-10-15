34843345cae10c02e3455ca660ea9087b6e19260
Alex Chen <achen831008@gmail.com>
2017-10-15 16:43:19 -0400

    new navbar, instagram feeds, block style.

--- scripts/controllers/app.controller.js
+++ scripts/controllers/app.controller.js
@@ -8,11 +8,11 @@
     appController.$inject = ['$scope', '$http', '$sce'];
 
     function appController($scope, $http, $sce) {
-        //https://api.instagram.com/v1/users/self/media/recent/?access_token=6214268740.c67ca99.7925875186c8449c84fdfe7cfea728d9
+        $scope.scroll = 0;
         var instagram_endpoint = 'https://api.instagram.com/v1/users/self/media/recent/';
-        var instagram_api_access_token = '6214268740.c67ca99.7925875186c8449c84fdfe7cfea728d9';
+        var instagram_api_access_token = '214634784.8a868cf.f7f8c489d0b64ecaa966e24cd1b4538b';
         var request_url = instagram_endpoint + '?access_token=' + instagram_api_access_token;
-        
+        $scope.feeds_limit = 6;
         request_url = $sce.trustAsResourceUrl(request_url);
         
         $http.get('/data/v2/app.json').then(function(rsp){
@@ -22,5 +22,13 @@
         $http.jsonp(request_url).then(function (rsp){
             $scope.feeds = rsp.data.data;
         });
+
+        //load more
+        //https://stackoverflow.com/questions/32211888/how-to-load-more-elements-with-angular
+
+        $scope.loadMore = function() {
+            var incremented = $scope.feeds_limit + 6;
+            $scope.feeds_limit = incremented > $scope.feeds.length ? $scope.feeds.length : incremented;
+        };
     }
 })();
