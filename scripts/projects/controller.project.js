508b109e8f4fb10db7724b9424d9e3fd840c0fe7
Alex Chen <achen831008@gmail.com>
2017-10-18 19:49:29 -0400

    Added loading partial for git projects view

--- scripts/projects/controller.project.js
+++ scripts/projects/controller.project.js
@@ -12,41 +12,39 @@
         init();
 
         function getRepos() {
-            return gitService.getRepos()
-                .then(function(data) {
-                    vm.github.repos = data;
-                    return vm.github.repos;
-                })
+            return gitService.getRepos().then(function(data) {
+                vm.github.repos = data;
+                return vm.github.repos;
+            });
         }
 
         function getCommits(repo) {
-            return gitService.getCommits(repo)
-                .then(function(data){
-                    return vm.github.commits = vm.github.commits.concat(data);
-                })
+            return gitService.getCommits(repo).then(function(data){
+                return vm.github.commits = vm.github.commits.concat(data);
+            });
         }
 
         function initGithub() {
             vm.github = {};
             vm.github.repos = [];
             vm.github.commits = [];
-            return getRepos()
-                .then(function(repos){
-                    _.each(repos, function(repo){
-                        getCommits(repo)
-                            .then(function(){
-                                vm.github.commits = _.filter(vm.github.commits, function(data){
-                                    return gitService.withinOneWeek(data.commit);
-                                })
-                            })
+            return getRepos().then(function(repos){
+                _.each(repos, function(repo){
+                    getCommits(repo).then(function(){
+                        vm.github.commits = _.filter(vm.github.commits, function(data){
+                            return gitService.withinOneWeek(data.commit);
+                        });
                     });
                 });
+            });
         };
 
         function init() {
             vm.scroll = 0;
             vm.loading = true;
-            initGithub();
+            initGithub().then(function(){
+                vm.loading = false;
+            });
         };
     }
 })();
